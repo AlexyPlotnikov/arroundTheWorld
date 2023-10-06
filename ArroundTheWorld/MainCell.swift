41234b68c2608c9aec2e51b54752e5f0e675e8ba
Алексей Плотников <plotnikov@rxgroup.ru>
2023-10-06 18:03:38 +0700

    Добавил бегущую строку

---
+++ ArroundTheWorld/MainCell.swift
@@ -0,0 +1,20 @@
+//
+//  MainCell.swift
+//  ArroundTheWorld
+//
+//  Created by Алексей Плотников on 06.10.2023.
+//
+
+import UIKit
+
+class MainCell: UITableViewCell {
+
+    @IBOutlet weak var scrollLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+ 
+}
